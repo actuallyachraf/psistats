c24006109964a17fa0735b3d21eae96ae3db51e8
actuallyachraf <actuallyachraf@gmail.com>
2021-12-26 23:04:42 +0100

    test:add prime assertion test

--- pkg/crypto/group_test.go
+++ pkg/crypto/group_test.go
@@ -9,9 +9,14 @@ import (
 
 func TestGroup(t *testing.T) {
 	t.Run("TestPrimeGroup", func(t *testing.T) {
+
+		assertOrderIsPrime := func(order *big.Int) bool {
+			return order.ProbablyPrime(256)
+		}
+
 		order, ok := new(big.Int).SetString(proto.MODPGroup1536, 16)
-		if !ok {
-			t.Fatal("TestPrimeGroup failed with error : bad string")
+		if !ok || !assertOrderIsPrime(order) {
+			t.Fatal("TestPrimeGroup failed with error : order is not prime")
 		}
 		pg := GenPrimeGroup(order)
 
@@ -20,7 +25,6 @@ func TestGroup(t *testing.T) {
 		}
 
 		for i := 0; i < 10; i++ {
-			t.Log(pg.RandomScalar())
 			if !isLessThan(pg.RandomScalar(), order) {
 				t.Fatal("TestPrimeGroup failed random scalar is higher than order")
 			}
