29d583a130cef665731fac260e09886c1c023924
actuallyachraf <actuallyachraf@gmail.com>
2021-12-26 23:13:17 +0100

    feat:implement hashing using sha256

---
+++ pkg/crypto/hash.go
@@ -0,0 +1,28 @@
+package crypto
+
+import (
+	"crypto/sha256"
+	"encoding/binary"
+	"math/big"
+)
+
+// H defines a hash function in this case SHA-256.
+func H(data []byte) []byte {
+	b := sha256.Sum256(data)
+	return b[:]
+}
+
+// HashBig returns the SHA-256 of a big integer.
+func HashBig(x *big.Int) []byte {
+	return H(x.Bytes())
+}
+
+// HashInt64 returns the SHA-256 of a <= 64-bit signed integer.
+// Integers are encoded using Go's proper Variable length encoding.
+// TODO:might be more sane to replace VarInt with a standard encoding
+// maybe LEB128 ?
+func HashInt64(x int64) []byte {
+	buf := make([]byte, binary.MaxVarintLen64)
+	binary.PutVarint(buf, x)
+	return H(buf)
+}
