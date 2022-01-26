c8ea9fd6627f34830a7c6fe64f5ab3124c133d1f
actuallyachraf <actuallyachraf@gmail.com>
2022-01-26 13:32:27 +0100

    test:use 2048-bit long keysize in Paillier to ensure scale compatibility with noise parameters

--- pkg/proto/proto_test.go
+++ pkg/proto/proto_test.go
@@ -19,21 +19,21 @@ func TestProtocolPrimitives(t *testing.T) {
 
 func TestProtocolInstance(t *testing.T) {
 	t.Run("TestPSIMean-SingleInstance", func(t *testing.T) {
-		a := []int{1, 2, 3, 4, 5, 6}
-		b := []int{0, 0, 3, 0, 5, 6}
-		bValues := []int{11, 13, 17, 21, 23, 29}
+		a := []int{1, 2, 3, 4, 5, 6, 8, 9}
+		b := []int{0, 0, 3, 0, 5, 6, 9, 10}
+		bValues := []int{11, 13, 17, 21, 23, 29, 31, 37}
 
 		// Setup
 		// Group aggrement
 		prime, _ := new(big.Int).SetString("57896044618658097711785492504343953926634992332820282019728792003956564819949", 10)
 		G := crypto.GenPrimeGroup(prime)
 		// A keypair
-		_, _, err := gaillier.GenerateKeyPair(rand.Reader, 128)
+		_, _, err := gaillier.GenerateKeyPair(rand.Reader, 2048)
 		if err != nil {
 			t.Fatal("[Alice]failed to generate keypair with error :", err)
 		}
 		// B keypair
-		Bpub, _, err := gaillier.GenerateKeyPair(rand.Reader, 128)
+		Bpub, Bpriv, err := gaillier.GenerateKeyPair(rand.Reader, 2048)
 		if err != nil {
 			t.Fatal("[Bob]failed to generate keypair with error :", err)
 		}
@@ -103,5 +103,93 @@ func TestProtocolInstance(t *testing.T) {
 		I := NaiveIntersect(encAshared, shuffledBshared)
 
 		t.Log("Alice computed PSI : ", I)
+
+		// Sample r
+		maxRange, _ := new(big.Int).SetString("179769313486231590772930519078902473361797697894230657273430081157732675805500963132708477322407536021120113879871393357658789768814416622492847430639474124377767893424865485276302219601246094119453082952085005768838150682342462881473913110540827237163350510684586298239947245938479716304835356329624224137216", 10)
+		r, err := rand.Int(rand.Reader, maxRange)
+		for err != nil || r.BitLen() != 1024 {
+			r, err = rand.Int(rand.Reader, maxRange)
+		}
+		k := int64(len(I))
+
+		// 0 <= r1 <= 2**128 - 1
+		r1UpperBound, _ := new(big.Int).SetString("340282366920938463463374607431768211455", 10)
+
+		// 2**511 <= r2 <= 2**512-1
+		r2LowerBound, _ := new(big.Int).SetString(
+			"6703903964971298549787012499102923063739682910296196688861780721860882015036773488400937149083451713845015929093243025426876941405973284973216824503042048", 10)
+		r2UpperBound, _ := new(big.Int).SetString("13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084095", 10)
+
+		// Sample r1
+		r1, err := rand.Int(rand.Reader, r1UpperBound)
+		if err != nil {
+			t.Fatal("failed to sample r1 with error :", err)
+		}
+		res := new(big.Int).SetInt64(0)
+		diff := new(big.Int).Sub(r1, r)
+		zero := new(big.Int).SetInt64(0)
+		bigK := new(big.Int).SetInt64(k)
+
+		res.Mod(diff, bigK)
+		for res.Cmp(zero) != 0 {
+			r1, _ = rand.Int(rand.Reader, r1UpperBound)
+			diff.Sub(r1, r)
+			res.Mod(diff, bigK)
+		}
+		t.Log("[+] Found valid r1 values : ", r1)
+
+		r2, _ := rand.Int(rand.Reader, r2UpperBound)
+
+		for r2.Cmp(r2LowerBound) < 0 {
+			r2, _ = rand.Int(rand.Reader, r2UpperBound)
+		}
+		t.Log("[+] Found valid r2 values : ", r2)
+
+		t.Log("[+] Additive Homomorphic Computation :")
+
+		//sumTi := new(big.Int).SetInt64(0)
+		// for _, idx := range I {
+		// 	if idx+1 == len(encBvalues) {
+		// 		t.Log("[+] Possible overflow returning...")
+		// 		break
+		// 	}
+		// 	tmp := gaillier.Add(Bpub, encBvalues[idx].Bytes(), encBvalues[idx+1].Bytes())
+		// 	sumTi.Add(sumTi, new(big.Int).SetBytes(tmp))
+		// }
+		//for _, idx := range I {
+		//
+		//	sumTi.Add(sumTi, encBvalues[idx])
+		//}
+		idx0 := I[0]
+		idx1 := I[1]
+		idx2 := I[2]
+		c1 := encBvalues[idx0]
+		c2 := encBvalues[idx1]
+		c3 := encBvalues[idx2]
+
+		c4 := gaillier.Add(Bpub, c1.Bytes(), c2.Bytes())
+		c5 := gaillier.Add(Bpub, c4, c3.Bytes())
+
+		constant := new(big.Int).Sub(r, r1)
+		constant.Div(constant, bigK)
+
+		rhs := gaillier.Mul(Bpub, c5, constant.Bytes())
+
+		encr := gaillier.AddConstant(Bpub, rhs, r2.Bytes())
+
+		t.Log("[+] Encrypted Message Size in Bits : ", len(encr)*8)
+
+		// B's decryption phase
+		decrypted, err := gaillier.Decrypt(Bpriv, encr)
+		if err != nil {
+			t.Fatal("[Bob] failed to decrypt encrypted value with error :", err)
+		}
+		t.Log("[+] R bitlen :", r.BitLen())
+		t.Log("[+] Decrypted Message :", new(big.Int).SetBytes(decrypted))
+		// Approximation
+		statistic := new(big.Float).Quo(new(big.Float).SetInt(new(big.Int).SetBytes(decrypted)), new(big.Float).SetInt(r))
+		t.Log("[+] Computed Summary Statistic :", statistic)
+		t.Log("[+] Plaintext Computed Summary Statistic :", (bValues[2]+bValues[4]+bValues[5])/len(I))
+
 	})
 }
